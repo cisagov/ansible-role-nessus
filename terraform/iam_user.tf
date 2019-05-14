38062f20d812c831fb219c61e0d6eefd9062c089
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2019-05-14 17:54:52 -0400

    Add Terraform code
    This Terraform code creates an AWS IAM user that is allowed to read
    from the bucket containing the Nessus Debian packages.  An IAM access
    key is created from this user, and that is what allows Terraform to
    fully test this Ansible role.

---
+++ terraform/iam_user.tf
@@ -0,0 +1,38 @@
+# The users being created
+resource "aws_iam_user" "user" {
+  name = "test-ansible-role-nessus"
+  tags = "${var.tags}"
+}
+
+# The IAM access key for the user
+resource "aws_iam_access_key" "key" {
+  user = "${aws_iam_user.user.name}"
+}
+
+# The S3 bucket of interest
+data "aws_s3_bucket" "bucket" {
+  bucket = "${var.bucket_name}"
+}
+
+# IAM policy document that allows reading from a specific S3 bucket.
+# This will be applied to the IAM user we are creating.
+data "aws_iam_policy_document" "s3_doc" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "s3:GetObject",
+    ]
+
+    resources = [
+      "${data.aws_s3_bucket.bucket.arn}/*",
+    ]
+  }
+}
+
+# The S3 policy for our IAM user that lets the user read from a
+# particular bucket.
+resource "aws_iam_user_policy" "s3_policy" {
+  user   = "${aws_iam_user.user.id}"
+  policy = "${data.aws_iam_policy_document.s3_doc.json}"
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,19 @@
+variable "aws_region" {
+  description = "The AWS region to deploy into (e.g. us-east-1)."
+  default     = "us-east-1"
+}
+
+variable "tags" {
+  type        = "map"
+  description = "Tags to apply to all AWS resources created"
+
+  default = {
+    Team        = "NCATS OIS - Development"
+    Application = "ansible-role-nessus testing"
+  }
+}
+
+variable "bucket_name" {
+  description = "The name of the bucket that the user should be allowed to read from."
+  default     = "ncats-3rd-party-packages"
+}
