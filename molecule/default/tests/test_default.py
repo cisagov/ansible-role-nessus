e40cb84ecba37a08429a0b8518422c1147aa7670
Jeremy Frasier <jeremy.frasier@gwe.cisa.dhs.gov>
2026-05-21 15:47:52 -0400

    Remove unnecessary list comprehensions from calls to all()
    This change gets rid of a couple of C419 errors from our flake8
    pre-commit linter.

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -22,9 +22,9 @@ def test_packages(host):
     debian_packages = ["expect", "jq", "nessus"]
     redhat_packages = ["expect", "jq", "Nessus"]
     if host.system_info.distribution in ["debian", "kali", "ubuntu"]:
-        assert all([host.package(pkg).is_installed for pkg in debian_packages])
+        assert all(host.package(pkg).is_installed for pkg in debian_packages)
     elif host.system_info.distribution in ["amzn", "fedora"]:
-        assert all([host.package(pkg).is_installed for pkg in redhat_packages])
+        assert all(host.package(pkg).is_installed for pkg in redhat_packages)
     else:
         assert False, f"Unknown distribution {host.system_info.distribution}"
 
