5106bea705a8eb99fccbe16f048bc25541cc2ec0
Jeremy Frasier <jeremy.frasier@gwe.cisa.dhs.gov>
2026-05-21 15:49:20 -0400

    Prefer raising AssertionError to assert False
    This change gets rid of a B011 error from our flake8 pre-commit
    linter.

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -26,7 +26,7 @@ def test_packages(host):
     elif host.system_info.distribution in ["amzn", "fedora"]:
         assert all(host.package(pkg).is_installed for pkg in redhat_packages)
     else:
-        assert False, f"Unknown distribution {host.system_info.distribution}"
+        raise AssertionError(f"Unknown distribution {host.system_info.distribution}")
 
 
 def test_nessus_version(host):
